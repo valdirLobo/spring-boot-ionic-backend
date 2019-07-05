79fd054038568699dcea8c2c56de3dfa4bbc76e4
“Junior <valdir_a@hotmail.com>
2019-07-05 11:06:53 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/valdir/cursomc/services/CategoriaService.java
+++ src/main/java/com/valdir/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.valdir.cursomc.domain.Categoria;
 import com.valdir.cursomc.repositories.CategoriaRepository;
+import com.valdir.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,10 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Categoria obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		
 		return obj;
 	}
