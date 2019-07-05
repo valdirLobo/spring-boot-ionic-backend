4a1c7b38598538bd74921f7525c80fe4dd481c57
“Junior <valdir_a@hotmail.com>
2019-07-05 14:52:34 -0300

    Endpoint /clientes/id disponivel

---
+++ src/main/java/com/valdir/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.valdir.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdir.cursomc.domain.Cliente;
+import com.valdir.cursomc.repositories.ClienteRepository;
+import com.valdir.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Cliente obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado id: " + id + ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+
+}
