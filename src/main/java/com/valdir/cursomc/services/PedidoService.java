7f6e47b42d1042d1302fa0ae9f152880d93f7459
“Junior <valdir_a@hotmail.com>
2019-07-08 17:20:42 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/valdir/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.valdir.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdir.cursomc.domain.Pedido;
+import com.valdir.cursomc.repositories.PedidoRepository;
+import com.valdir.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Pedido obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado id: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		
+		return obj;
+	}
+
+}
