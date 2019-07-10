b14b82a0462a97423b676b455a3f191bc0c02ae3
“Junior <valdir_a@hotmail.com>
2019-07-10 16:07:29 -0300

    Listando todas categorias com DTO

--- src/main/java/com/valdir/cursomc/services/CategoriaService.java
+++ src/main/java/com/valdir/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.valdir.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,6 +46,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivle excluir uma categoria que possui produtos");
 		}
 		
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
