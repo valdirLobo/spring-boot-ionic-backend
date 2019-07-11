6c758d919e24a08059b156391eec3de5453d9ae8
“Junior <valdir_a@hotmail.com>
2019-07-11 16:54:38 -0300

    Put, delete e e GET para cliente

--- src/main/java/com/valdir/cursomc/services/CategoriaService.java
+++ src/main/java/com/valdir/cursomc/services/CategoriaService.java
@@ -47,7 +47,7 @@ public class CategoriaService {
 		try {
 			repo.delete(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivle excluir uma categoria que possui produtos");
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 		
 	}
