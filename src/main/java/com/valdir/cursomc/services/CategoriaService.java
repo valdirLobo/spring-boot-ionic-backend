fd73d4054971f6056fb15e450e18f58c25edcab7
“Junior <valdir_a@hotmail.com>
2019-07-12 10:46:02 -0300

    Correcoes em CategoriaService e construtor de Pagamento

--- src/main/java/com/valdir/cursomc/services/CategoriaService.java
+++ src/main/java/com/valdir/cursomc/services/CategoriaService.java
@@ -38,8 +38,11 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -64,6 +67,12 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
+		
 	}
 
 }
